2eab5814f6a2700e9ea36fa3fc070307e6aa5521
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-01-14 09:34:00 -0500

    Initial commit

---
+++ src/main/java/com/all/software/ereservas/EReservasApplication.java
@@ -0,0 +1,14 @@
+package com.all.software.ereservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EReservasApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/all/software/ereservas/modelo/Reserva.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.all.software.ereservas.modelo;
+
+/**
+ * Clase que
+ * @author pedrhiga
+ *
+ */
+
+
+public class Reserva {
+
+}
