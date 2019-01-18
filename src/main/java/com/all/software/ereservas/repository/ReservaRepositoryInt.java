684e5cda3a4cfaff5498cbb10312c733d2824f99
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-01-18 14:49:09 -0500

    Crear interfaces para el consumo del repository

---
+++ src/main/java/com/all/software/ereservas/repository/ReservaRepositoryInt.java
@@ -0,0 +1,9 @@
+package com.all.software.ereservas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.all.software.ereservas.modelo.Reserva;
+
+public interface ReservaRepositoryInt extends JpaRepository<Reserva, String> {
+
+}
